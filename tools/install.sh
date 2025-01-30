30316a1b17e55eea30e74324b51c1cf0600883a2
Bernard Xiong <bernard.xiong@gmail.com>
2025-01-30 08:00:23 +0000

    Add install.sh for codespace

---
+++ tools/install.sh
@@ -0,0 +1,10 @@
+# !/bin/sh
+
+# 在Linux下准备smart-build的环境
+sudo apt update
+sudo apt install python3 python3-pip parted unzip dosfstools -y
+pip install scons requests tqdm kconfiglib
+
+sudo add-apt-repository ppa:xmake-io/xmake
+sudo apt update
+sudo apt install xmake
